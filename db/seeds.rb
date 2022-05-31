bd4aaf5152e04fbe053928692a75719718823854
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-05-31 14:08:59 +0200

    okay ish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all # explain thoroughly
+
+# used new instead of create ouch !
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Movies::HarryPotter.spell,
+    content: Faker::Quote.famous_last_words
+  )
+  article.save!
+end
+
+# 'end of the seed' the rake worked but couldn't see the text
